2923d72724a38969de9459ab5d049d03ce6c3ee3
gaizkazaqhariou <gaizkapolinema@gmail.com>
2020-03-26 20:12:45 +0700

    making login activity

--- app/src/main/java/id/ac/polinema/aplikasikas/Application.java
+++ app/src/main/java/id/ac/polinema/aplikasikas/Application.java
@@ -1,17 +1,24 @@
 package id.ac.polinema.aplikasikas;
 
 import id.ac.polinema.aplikasikas.model.Account;
+import id.ac.polinema.aplikasikas.model.Session;
 
 public class Application extends android.app.Application {
     private static Account account;
+    private static Session session;
 
     public static Account getAccount() {
         return account;
     }
 
+    public static Session getSession() {
+        return session;
+    }
+
     @Override
     public void onCreate() {
         super.onCreate();
-        account = new Account("Gaizka");
+        account = new Account("gazka");
+        session = new Session(this);
     }
 }
